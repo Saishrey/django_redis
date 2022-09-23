1bd7f2124b960e177a523d8b09bffe5f117a6522
Shreyas Naik <naiksp16@gmail.com>
2022-09-23 10:31:56 +0530

    new_commit

--- core/home/views.py
+++ core/home/views.py
@@ -8,14 +8,16 @@ def home(request):
 
     db = None
 
+
     if cache.get('fruits'):
         payload = cache.get('fruits')
         db = 'redis'
+        print('Redis_server: cache hit')
     else:
         objs = Fruits.objects.all()
         payload = [obj.fruit_name for obj in objs]
         db = 'sqlite'
-
+        print('Redis_server: cache miss')
         cache.set('fruits', payload)
 
     return JsonResponse({'status' : 200, 'db' : db, 'data' : payload})
